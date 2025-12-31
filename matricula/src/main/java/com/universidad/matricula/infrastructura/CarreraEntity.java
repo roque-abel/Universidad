df0a2aa2dfe7783ba0b110cca2a3707b498e66af
roque-abel <roqueabel0409@gmail.com>
2025-12-30 23:38:59 -0600

    feat(add): integrando entidades: AlumnoEntity, CarreraEntity

---
+++ matricula/src/main/java/com/universidad/matricula/infrastructura/CarreraEntity.java
@@ -0,0 +1,7 @@
+package com.universidad.matricula.infrastructura;
+
+public class CarreraEntity {
+    private Integer id;
+    private String nombre;
+    private
+}
---
+++ matricula/src/main/java/com/universidad/matricula/infrastructura/GeneroEnum.java
@@ -0,0 +1,26 @@
+package com.universidad.matricula.infrastructura;
+
+import java.util.Objects;
+
+public enum GeneroEnum {
+    MASCULINO(1), FEMENINO(2);
+
+    private Integer genero;
+
+    GeneroEnum(Integer genero) {
+        this.genero = genero;
+    }
+
+    public int getGenero() {
+        return genero;
+    }
+
+    public GeneroEnum valueOf(Integer genero) {
+        for (GeneroEnum generoEnum : GeneroEnum.values()) {
+            if (Objects.equals(genero, generoEnum.getGenero())) {
+                return generoEnum;
+            }
+        }
+        throw new IllegalArgumentException("No enum" + genero);
+    }
+}
